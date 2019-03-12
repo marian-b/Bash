78416aa7ffb3d000ddef658c11629e914107aa45
Marian <44984312+marian-b@users.noreply.github.com>
2019-03-12 23:29:29 +0200

    An interactive script to get a monitoring report through ssh from a linux server.

---
+++ Report.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Afiseaza cererea de introducere a datelor.
+
+read -p "Numele dumneavoastra de utilizator:" homeuser ;
+
+echo "Introduceti datele de conectare pentru dispozitivul dorit.";
+
+read -p "Adresa IP a dispozitivului:" ipvar;
+
+read -p "Nume utilizator:" uservar;
+
+read -sp "Parola utilizator:" passvar;
+
+echo "";
+
+echo "Se incearca conectarea..";
+
+sshpass -p $passvar ssh $uservar@$ipvar <<EOF
+
+export TERM=xterm ;
+
+echo $passvar | sudo -S apt-get -y install htop ;
+
+echo $passvar | sudo -S apt-get install aha ;
+
+echo -q | htop | aha --black --line-fix > /home/$uservar/Desktop/Raport.html;
+
+EOF
+
+sshpass -p $passvar scp $uservar@$ipvar:/home/$uservar/Desktop/Raport.html /home/$homeuser/Desktop ;
+
+echo "Va pregatim raportul.." ;
+
+sleep 2s ;
+
+firefox /home/$homeuser/Desktop/Raport.html ; 
+
+
